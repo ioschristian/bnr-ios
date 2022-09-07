72207da60d9c34f97c7ee576b7503259e2e31b79
Christian Tisby <christian.d.tisby@gmail.com>
2022-09-07 12:07:26 -0400

    Updating World Trotter to use programmatic views

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Christian Tisby on 8/29/22.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+    }
+}
